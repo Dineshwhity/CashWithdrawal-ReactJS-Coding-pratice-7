00cb515413560ffe29e1c93c2837e3267198b5bd
Dineshwhity <dineshwhity>
2023-07-12 06:43:49 +0000

    Cash

---
+++ src/components/DenominationItem/index.js
@@ -0,0 +1,18 @@
+// Write your code here
+import "./index.css"
+const DenominationItem=props=>{
+    const {denominationDetails,updatedBalance}=props
+    const{value}=DeviceOrientationEvent
+
+    const onClickDenomination=()=>{
+        updatedBalance(value)
+    }
+    return(
+        <li className="Denomination-list">
+        <button type="button" className="button" onClick={onClickDenomination}>{value}
+        </button>
+        </li>
+    )
+}
+
+export default DenominationItem
